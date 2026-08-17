f80e5911f68af498708380778ea6ca1048e1d794
PreethikaSubramani <preethikasubramani02@gmail.com>
2026-08-17 13:09:01 +0100

    chore: add initial project files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LandingPage from './pages/LandingPage';
+
+
+const App = () => {
+  return (
+
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,29 @@
+import "./LandingPage.css"
+
+const LandingPage = () => {
+  return (
+     <header className="navbar">
+      <div className="container navbar__container">
+
+        <a href="/" className="navbar__brand">
+          <span className="navbar__logo">GRFI</span>
+          <span className="navbar__tagline">
+            Get Ready For Interview
+          </span>
+        </a>
+
+        <nav className="navbar__links">
+          <a href="#how-it-works">How it works</a>
+          <a href="#features">Features</a>
+
+          <button className="navbar__button">
+            Start Interview
+          </button>
+        </nav>
+
+      </div>
+    </header>
+  )
+}
+
+export default LandingPage
